0f8ed594fc087d66a19611d7cf22c20db6c204c0
swing <swing@live.com>
2020-02-20 11:34:28 +0800

    add:init integration

---
+++ framework/src/Volo.Abp.EPPlus/Volo/Abp/EPPlus/AbpFormFileExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+namespace Microsoft.AspNetCore.Http
+{
+    public static class AbpFormFileExtensions
+    {
+        // public static List<TEntity> MapTo<TEntity>(this IFormFile file, Func<ExcelWorksheet, List<TEntity>> mapper)
+        // {
+
+            // if (file == null || file.Length <= 0)
+            // {
+            //     throw new ArgumentNullException("File not null");
+            // }
+
+            // if (
+            //     new[] { ".xlsx", ".xls" }.All(ext => !Path.GetExtension(formFile.FileName)
+            //     .Equals(ext, StringComparison.OrdinalIgnoreCase))
+            // )
+            // {
+            //     throw new ArgumentNullException("Not support file Extension");
+            // }
+
+
+            // using (var stream = new MemoryStream())
+            // {
+            //     await formFile.CopyToAsync(stream, cancellationToken);
+
+            //     using (var package = new ExcelPackage(stream))
+            //     {
+            //         ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+            //         var rowCount = worksheet.Dimension.Rows;
+
+            //         return mapper(worksheet);
+            //     }
+            // }
+        // }
+    }
+}
